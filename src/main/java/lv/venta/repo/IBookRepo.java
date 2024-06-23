ab556fe524fbcaacaca024db46f6b87af46fd9ab
s22zapoandr <151927971+s22zapoandr@users.noreply.github.com>
2024-06-23 12:41:04 +0300

    Update IBookRepo.java
    Added autoimplemented functions for search

--- src/main/java/lv/venta/repo/IBookRepo.java
+++ src/main/java/lv/venta/repo/IBookRepo.java
@@ -1,8 +1,29 @@
 package lv.venta.repo;
 
 import lv.venta.model.Book;
+import lv.venta.model.Condition;
+import lv.venta.model.Genre;
+import lv.venta.model.Rarity;
+
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IBookRepo extends JpaRepository<Book, Long> {
 	
+	ArrayList<Book> findCurrentTakenBookListByIdR(long idR);
+	
+	ArrayList<Book> findBookHistoryByIdR(long idR);
+	
+	ArrayList<Book> findWrittenBooksByIdA(long idA);
+	
+	ArrayList<Book> findByGenre(Genre genre);
+	
+	ArrayList<Book> findByRarity(Rarity rarity);
+	
+	ArrayList<Book> findByCondition(Condition condition);
+	
+	ArrayList<Book> findByRating(int rating);
+	
+	ArrayList<Book> findByYear(long year);
 }
