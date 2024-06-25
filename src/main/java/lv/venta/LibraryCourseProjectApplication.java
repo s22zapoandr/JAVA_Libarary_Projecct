556d414e197e4dcca4f0624dfad3f0122e981561
igorsgonc <156427776+Freezewind12@users.noreply.github.com>
2024-06-25 11:27:38 +0300

    Update LibraryCourseProjectApplication.java

--- src/main/java/lv/venta/LibraryCourseProjectApplication.java
+++ src/main/java/lv/venta/LibraryCourseProjectApplication.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 
 import lv.venta.model.Author;
 import lv.venta.model.Book;
+import lv.venta.model.Condition;
 import lv.venta.model.Genre;
 import lv.venta.model.LibraryDepartment;
+import lv.venta.model.Rarity;
 import lv.venta.repo.IAuthorRepo;
 import lv.venta.repo.IBookRepo;
 
@@ -33,13 +35,13 @@ public class LibraryCourseProjectApplication {
     @Bean
     public CommandLineRunner dataInitializer(IAuthorRepo authorRepo, IBookRepo bookRepo) {
         return args -> {
-            Author author1 = new Author("J.K. Rowling");
-            Author author2 = new Author("J.R.R. Tolkien");
+            Author author1 = new Author("Joanne", "Rowling", Genre.Fantasy);
+            Author author2 = new Author("John", "Tolkien", Genre.Fantasy);
             authorRepo.save(author1);
             authorRepo.save(author2);
 
-            Book book1 = new Book("Harry Potter and the Philosopher's Stone", author1, Genre.Fantasy);
-            Book book2 = new Book("The Hobbit", author2, Genre.Fantasy);
+            Book book1 = new Book("Harry Potter and the Philosopher's Stone", author1, 9, Condition.Good, Rarity.Common , 25, 1997);
+            Book book2 = new Book("The Hobbit", author2, 8, Condition.Moderate, Rarity.Rare , 13, 1937);
             bookRepo.save(book1);
             bookRepo.save(book2);
         };
