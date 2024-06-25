5caaa00823c0bcb7f2b91b2f776bcc7b52aad937
s22zapoandr <151927971+s22zapoandr@users.noreply.github.com>
2024-06-25 11:57:49 +0300

    Updated AuthorController

--- src/main/java/lv/venta/controller/AuthorController.java
+++ src/main/java/lv/venta/controller/AuthorController.java
@@ -1,34 +1,100 @@
 package lv.venta.controller;
 
 import lv.venta.model.Author;
-import lv.venta.repo.IAuthorRepo;
+import lv.venta.model.Genre;
+import lv.venta.service.AuthorCRUDService;
+
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/authors")
+@RequestMapping("/author")
 public class AuthorController {
 
+    
     @Autowired
-    private IAuthorRepo authorRepo;
+    private AuthorCRUDService authorService;
 
-    @GetMapping
-    public String listAuthors(Model model) {
-        model.addAttribute("authors", authorRepo.findAll());
-        return "authors";
+    @GetMapping("/show/all/")
+    public String getAllAuthors(Model model) {
+        try {
+            ArrayList<Author> drivers = authorService.selectAllAuthors();
+            model.addAttribute("drivers", drivers);
+            return "author-show-all";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
     }
 
+    @GetMapping("/show/all/{idA}")
+    public String getAuthorById(@PathVariable("idA") Long idA, Model model) {
+        try {
+            Author author = authorService.selectAuthorById(idA);
+            model.addAttribute("author", author);
+            return "author-one-page";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
+    }
+
+
+    @GetMapping("/remove/{idP}")
+    public String removeAuthorById(@PathVariable("idA") Long idA, Model model) {
+        try {
+            authorService.deleteAuthorById(idA);
+            return "redirect:/author/show/all";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
+    }
+
+
     @GetMapping("/add")
-    public String showAddAuthorForm(Model model) {
-        model.addAttribute("author", new Author());
-        return "add-author";
+    public String getAuthorForm(Model model) {
+        model.addAttribute("Author", new Author());
+        return "author-add-page";
     }
 
     @PostMapping("/add")
-    public String addAuthor(@ModelAttribute Author author) {
-        authorRepo.save(author);
-        return "redirect:/authors";
+    public String addAuthor(@ModelAttribute String name, String surname, Genre genre, Model model) {
+        try {
+            authorService.insertNewAuthor(name, surname, genre);
+            return "redirect:/driver/show/all";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
     }
-}
+
+   
+    @GetMapping("/update/{idP}")
+    public String getUpdateDriverForm(@PathVariable("idA") Long idA, Model model) {
+        try {
+            Author author = authorService.selectAuthorById(idA);
+            model.addAttribute("Author", author);
+            return "driver-update-page";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+        }
+    }
+
+    @PostMapping("/update/{idA}")
+    public String updateAuthorById(@PathVariable("idA") Long idA, String name, String surname, Genre genre, Model model) {
+        try {
+            authorService.updateAuthorById(idA, name,  surname, genre);
+            return "redirect:/driver/show/all";
+        } catch (Exception e) {
+            model.addAttribute("error", e.getMessage());
+            return "error-page";
+     
+        }
+}}
+
