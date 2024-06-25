55ec8d45b3537ec115fb757ab6d9d2378d3495a9
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-06-25 12:05:02 +0300

    Updated IAuthorRepo

--- src/main/java/lv/venta/repo/IAuthorRepo.java
+++ src/main/java/lv/venta/repo/IAuthorRepo.java
@@ -1,8 +1,14 @@
 package lv.venta.repo;
 
 import lv.venta.model.Author;
+import lv.venta.model.Genre;
+
+import java.util.ArrayList;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IAuthorRepo extends JpaRepository<Author, Long> {
+
+	ArrayList<Author> findAllByGenre(Genre genre);
 	
 }
