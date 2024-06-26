faa9d9b16aadd8a803308066b8c9915607c3a4df
s22zapoandr <151927971+s22zapoandr@users.noreply.github.com>
2024-06-27 01:04:56 +0300

    Update LibraryCourseProjectApplication.java

--- src/main/java/lv/venta/LibraryCourseProjectApplication.java
+++ src/main/java/lv/venta/LibraryCourseProjectApplication.java
@@ -3,6 +3,7 @@ package lv.venta;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
