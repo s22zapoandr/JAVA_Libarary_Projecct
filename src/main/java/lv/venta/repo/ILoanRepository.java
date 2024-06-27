02b08852a376fd61dbaff145376e3f53c657bdf9
igorsgonc <156427776+Freezewind12@users.noreply.github.com>
2024-06-27 11:48:00 +0300

    Update ILoanRepository.java

--- src/main/java/lv/venta/repo/ILoanRepository.java
+++ src/main/java/lv/venta/repo/ILoanRepository.java
@@ -1,12 +1,12 @@
 package lv.venta.repo;
 
 import lv.venta.model.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public interface ILoanRepository extends JpaRepository<Loan, Long> {
+public interface ILoanRepository extends CrudRepository<Loan, Long> {
     ArrayList<Loan> findByDueDateBefore(LocalDate date);
 }
 
